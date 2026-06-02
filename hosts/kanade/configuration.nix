50d997117d09074b481192bad83b9507d2117d23
Jake <jake@skibidi.moe>
2026-06-02 23:20:06 +0300

    lol

--- hosts/kanade/configuration.nix
+++ hosts/kanade/configuration.nix
@@ -3,11 +3,47 @@
   pkgs,
   ...
 }: {
-  networking.hostName = "kanade"; #
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  networking.hostName = "kanade";
+  networking.networkmanager.enable = true;
 
   fileSystems."/".options = ["noatime" "discard"];
   boot.tmp.useTmpfs = true;
   services.journald.extraConfig = "SystemMaxUse=100M";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Helsinki";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   nix = {
     settings.auto-optimise-store = true;
